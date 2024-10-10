de655e7365c791e911f4aac04ab79a2ff1bf50a5
grurquijo <grurquijo@gmail.com>
2024-10-10 13:47:43 -0700

    add exercise:create loop code to response_collection_python

--- Experiment1/response_collection_python.py
+++ Experiment1/response_collection_python.py
@@ -16,7 +16,7 @@ from psychopy import visual, core, event
 fullscr = False
 screen_size = [600,600]
 max_wait = 3 
-method = 'waitKeys' # 'getKeys'
+method = 'getKeys' # 'waitKeys'  
 win = visual.Window(size=screen_size, 
                     color=(0.5,0.5,0.5),
                     fullscr=fullscr, 
@@ -34,8 +34,18 @@ win.flip()
 t0 = core.getTime()
 
 # Exercise: Display something here (a dot, an image), and time responses from there
+trial_number = 5
 
 # Exercise: Create a loop getting responses after each presentation
+while trial_number > 0 :
+    circ_pos = (0,0)
+    circ_stim = visual.Circle(win, radius=12,
+                            units='pix',
+                            fillColor=(1.0, 0.5, 0.0),
+                            pos=circ_pos)
+    circ_stim.draw()
+    win.flip()
+    trial_number -= 1
 
 if method == 'waitKeys':
     key_out = event.waitKeys(maxWait=max_wait, keyList=['left','right','q'])
@@ -53,7 +63,8 @@ if (key_out is not None) and (len(key_out) > 0):
 else:
     print('Reponse timed out!')
 print('\n\n\n')
-
 win.close()
 core.quit()
 
+
+# %%
