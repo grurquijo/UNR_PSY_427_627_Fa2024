f0b6f57b6a9d438732640b538b180b5478cf66e5
grurquijo <grurquijo@gmail.com>
2024-11-18 17:03:58 -0800

    update plan

--- Assignments/Experiment2/experiment2_functions.py
+++ Assignments/Experiment2/experiment2_functions.py
@@ -1,22 +1,76 @@
 # Name: Giselle Urquijo
-# Date modified: 11/16/2024
+# Date modified: 11/18/2024
 
 import os
 import sys
+import json
 import psychopy
 import matplotlib.pyplot as plt
 
 from psychopy import visual, core, event
 from psychopy.hardware import keyboard
 
+
+# Json file management
+def write_json(data, filename:str):
+    with open(filename, "w") as f:
+        json.dump(data, f, indent=2)
+
+def open_file(filename:str):
+    with open(filename) as f:
+        load_file = json.load(f)
+
+    return load_file
+
+def does_file_exist(filename:str):
+    dir_list = os.listdir('./Experiments/')
+    for f in dir_list:
+        if f == filename:
+            return True
+    
+    return False
+
+def gen_new_filename():
+    if not does_file_exist("Experiment0.json"):
+        return "Experiment0.json"
+    else:
+        for i in range(100):
+            i += 1
+            if not does_file_exist('Experiment' + str(i) + '.json'):
+                return 'Experiment' + str(i) + '.json'
+            
+    return False
+
+
+def list_files(path:str) -> list:
+    '''
+    Finds and appends all files in a specified directory to a list and returns it.
+    '''
+    file_list = []
+    dir_list = os.listdir(path)
+    dir_string = ""
+    print("--- List of files ---")
+    for f in dir_list:
+        dir_string += f + " "
+        file_list.append(f)
+        print('\033[92m' + f + '\033[0m')
+    print("--- End of list ---")
+
+    return file_list
+
 # (1) Get all images that match a certain pattern (i.e. contain some sub-string) from a directory. The directory and sub-string should be the inputs, and a list or cell array of file names should be the output
 
-def find_files(path:str, empty_list:list, file_name:list):
+def find_files(path:str, empty_list:list, file_name:list) -> list:
+    '''
+    Look for given files in a path. Returns list off all files found.
+    '''
     for x in file_name:
         for root, dirs, files in os.walk(path):
             for file in files:
                 if file.startswith(x):
-                    empty_list.append(os.path.join(path, file))    
+                    empty_list.append(os.path.join(path, file))
+    
+    return empty_list
 
 # (2) Collect responses
 
@@ -26,16 +80,17 @@ def check_keypress(key_list:list, key_in):
         print(key_out)
     elif key_in[0] == key_list[1]:
         raise Exception ('Quit Experiment')
+    
     return key_out, key_time
 
 # (3) Set up the screen.
-def screen_setup(screen_size:list, fullscreen:False, screen_color):
+def screen_setup(screen_size:list, fullscreen:False, screen_color:tuple = (0.5,0.5,0.5)):
     if fullscreen is True:
         win = visual.Window(color=screen_color,
                             fullscr=True,
                             units='pix',)
     else:
-        win = visual.Window(color=(0.5,0.5,0.5),
+        win = visual.Window(color=screen_color,
                     fullscr=screen_size,
                     units='pix',) 
     return win
@@ -63,10 +118,10 @@ def screen_setup(screen_size:list, fullscreen:False, screen_color):
 
 # The experiment should contain 30 trials.
 # what exactly is in a trial? is one trial just one judgement or are we doing categories as trials??
-def trial(trial_num:int, stim_list:list, stim_duration:float, win=win):
+def trial(trial_num:int, stim_list:list, stim_duration:float, win):
     while trial_num >= 0:
-        img1 = visual.ImageStim(win, random.choice(stim_list), pos=(-200,0), size=(300,300), units="pix")
-        img2 = visual.ImageStim(win, random.choice(stim_list), pos=(200,0), size=(300,300), units="pix")
+        img1 = visual.ImageStim(win, stim_list, pos=(-200,0), size=(300,300), units="pix")
+        img2 = visual.ImageStim(win, stim_list, pos=(200,0), size=(300,300), units="pix")
         img1.draw()
         img2.draw()
         win.flip()
--- Assignments/Experiment2/experiment2_plan.py
+++ Assignments/Experiment2/experiment2_plan.py
@@ -1,39 +1,88 @@
+# Name: Giselle Urquijo
+# Date modified: 11/18/2024
+
 import os
 import json
 
 from psychopy import gui
+from experiment2_functions import *
+
+
 # use dialogue box to create experiments
     
-# you can get dialogue from dict and responses save to a list
-# json writer and parser DO IT BITCH THIS IS THE PLACE TO DO IT
+# you can get dialogue from dict and responses save to a list >>saves as a dict and can be passed into json 
+# we're using json just DO IT BITCH
 # save plan to json bc it can be opened with other languages and pickle can be big
 
 # use dialogue box to get information about:
 #       - use default trial, edit existing, or create new
 #           > unique name or generated name
-choose_trial = {'Choose one of the following': ('default trial', 'choose existing trial', 'create new trial', 'edit existing trial')}
+choose_experiment = {'Choose one of the following': ('default experiment', 
+                                                     'choose existing experiment', 
+                                                     'create new experiment', 
+                                                     'edit existing experiment')}
 
-dlg_win_one = gui.DlgFromDict(dictionary=choose_trial, title='Experiment 2', sortKeys=False)
-#       - name of folder where stimuli exist 
-#           > warn that stimuli should all be in the same folder and uniquely named 
-#           > will search for directory location
-stimuli_loc = {'Name of folder(s) where stimuli is located':'floc_stimuli'}
+dlg_win_one = gui.DlgFromDict(dictionary=choose_experiment, title='Experiment 2', sortKeys=False, copyDict=True, )
 
-dlg_win_two = gui.DlgFromDict(dictionary=stimuli_loc, title='Experiment 2',sortKeys=False)
-#       - for new trial (define what a trial consists of):
-#           > screen size and screen color
-#           > number of trials
-#           > because this is a two alternative forced comparison experiment 
-#               >> what two categories are we comparing/are we comparing just from the same category
-#           > duration of stimulus and max_wait time
-#           
-create_new_trial = {'Screen size':'True, False, or [300,300]',
-                    'Screen color':'[0.5,0.5,0.5]', 
-                    'Number of trials':0, 
-                    'Category names':'faces', 
-                    'Stimulus duration (s)':0, 
-                    'Max wait time (s)':0}
+tst = dlg_win_one.dictionary['Choose one of the following']
 
-dlg_win_three = gui.DlgFromDict(dictionary=create_new_trial, title='Experiment 2',sortKeys=False, copyDict=True)
+# im so sad i cant use match case here since its only available in python 3.10 :(
+if tst == 'default experiment':
+    print('1')
+    stimuli_loc = {'Pathname of stimuli folder':'C:/Users/Giselle/Documents/python/UNR/fLoc_stimuli'}
+    dlg_default_experiment = gui.DlgFromDict(dictionary=stimuli_loc, title='Experiment 2', copyDict=True)
 
-print(dlg_win_three.dictionary)
+    fname = dlg_default_experiment.dictionary
+    temp = open_file(filename='./Experiments/default_experiment.json')
+    temp["Stimuli location"].append(fname['Pathname of stmuli folder'])
+    write_json(data=temp, filename='./Experiments/default_experiment.json')
+    
+elif tst == 'choose existing experiment':
+    print('2')
+    saved_trials = {'choose one': []}
+    f = list_files(path='./Experiments')
+    for i,idx in enumerate(f):
+        saved_trials['choose one'].append(f[i])
+    
+    dlg_choose_existing_experiment = gui.DlgFromDict(dictionary=saved_trials, title='Experiment2', sortKeys=False, copyDict=True)
+
+elif tst == 'create new experiment':
+    print('3')
+    fname = gen_new_filename()
+    #       - name of folder where stimuli exist 
+    #           > warn that stimuli should all be in the same folder and uniquely named 
+    #           > will search for directory location
+    stimuli_loc = {'Pathname of stimuli folder':'C:/Users/Giselle/Documents/python/UNR/fLoc_stimuli'}
+
+    dlg_win_two = gui.DlgFromDict(dictionary=stimuli_loc, title='Experiment 2',sortKeys=False, copyDict=True)
+    #       - for new trial (define what a trial consists of):
+    #           > screen size and screen color
+    #           > number of trials
+    #           > because this is a two alternative forced comparison experiment 
+    #               >> what two categories are we comparing/are we comparing just from the same category
+    #           > duration of stimulus and max_wait time
+    #           
+    create_new_trial = {'Screen size (pixels)':'True, False, or [height,width]',
+                        'Screen color':'(0.5,0.5,0.5)', 
+                        'Number of trials':0, 
+                        'Category names':'faces', 
+                        'Stimulus duration (s)':0, 
+                        'Max wait time (s)':0}
+
+    dlg_win_three = gui.DlgFromDict(dictionary=create_new_trial, title='Experiment 2',sortKeys=False, copyDict=True)
+
+    write_json(data=dlg_win_three.dictionary, filename='./Experiments/'+fname)
+
+elif tst == 'edit existing experiment':
+    print('4')
+    saved_trials = {'choose one': []}
+    f = list_files(path='./Experiments')
+    for i,idx in enumerate(f):
+        saved_trials['choose one'].append(f[i])
+    dlg_edit_existing_experiment = gui.DlgFromDict(dictionary=saved_trials, title='Experiment2', sortKeys=False, copyDict=True)
+
+    chosen_experiment = dlg_edit_existing_experiment.dictionary
+
+    edit_experiment = open_file(filename='./Experiments/' + chosen_experiment['choose one'])
+
+    dlg_edit_existing_experiment = gui.DlgFromDict(dictionary=edit_experiment)
